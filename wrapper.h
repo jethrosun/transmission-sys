9db62591388c868ff530bf98338f8bf289001c14
Daniel McCrevan <danielmccrevan@gmail.com>
2019-01-16 10:56:26 -0500

    Added extra header files from before

--- wrapper.h
+++ wrapper.h
@@ -1,8 +1,13 @@
 #include "vendor/libtransmission/transmission.h"
+#include "vendor/libtransmission/error-types.h"
 #include "vendor/libtransmission/error.h"
 #include "vendor/libtransmission/file.h"
 #include "vendor/libtransmission/tr-getopt.h"
 #include "vendor/libtransmission/utils.h" /* tr_wait_msec */
 #include "vendor/libtransmission/variant.h"
 #include "vendor/libtransmission/makemeta.h"
+#include "vendor/libtransmission/quark.h"
+#include "vendor/libtransmission/rpcimpl.h"
 // #include "vendor/libtransmission/version.h"
+// #include "vendor/libtransmission/web.h" /* tr_webRun */
+
